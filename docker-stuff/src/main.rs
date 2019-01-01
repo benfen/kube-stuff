a627d8c28b05a6064efa90e03778cf38221bf52b
Ben <benfenny@gmail.com>
2019-01-01 15:06:44 -0600

    Auto kill server
    Modified the server to die after five requests for testing.

--- docker-stuff/src/main.rs
+++ docker-stuff/src/main.rs
@@ -1,5 +1,6 @@
 use iron::prelude::*;
 use iron::status;
+use std::{thread, time};
 
 static mut NUM: i32 = 0;
 
@@ -7,7 +8,16 @@ fn handler(_: &mut Request) -> IronResult<Response> {
     unsafe {
         let resp = format!("Hello: {}", NUM);
         NUM += 1;
-        Ok(Response::with((status::Ok, resp)))
+
+        if NUM == 5 {
+            thread::spawn(|| {
+                thread::sleep(time::Duration::from_millis(100));
+                ::std::process::exit(0);
+            });
+            Ok(Response::with((status::Ok, "Exiting")))
+        } else {
+            Ok(Response::with((status::Ok, resp)))
+        }
     }
 }
 
